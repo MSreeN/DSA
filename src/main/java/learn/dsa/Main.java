c5dbbb8694b870ded2a447d1ab21e4b8d8d72967
MSreeN <khyathimnsree369@gmail.com>
2024-07-05 22:54:21 +0530

    Working on strings

--- src/main/java/learn/dsa/Main.java
+++ src/main/java/learn/dsa/Main.java
@@ -1,7 +1,33 @@
 package learn.dsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sample("580",new String[]{});
+    }
+
+    public static void sample(String siteId,String[] ppay, String... args){
+        List<String> ppayList = List.of(ppay);
+        List<String> xstageId = new ArrayList<>();
+        for(String a : args){
+            String siteIdIn = siteId;
+            int noOfZeroes=0;
+            if(a.length() < 12){
+                noOfZeroes = (12 - a.length());
+            }
+            siteIdIn = String.format("%-"+noOfZeroes+"s", siteIdIn).replace(" ","0");
+            xstageId.add(siteIdIn+a);
+        }
+
+        for(String id : ppayList){
+            if(!xstageId.contains(id)){
+                System.out.println(id);
+            }
+        }
+
     }
 }
+
+
