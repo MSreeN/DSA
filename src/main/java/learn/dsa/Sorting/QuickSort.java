d2dbee398cf950edcc77b6983f18db23882c1a35
MSreeN <khyathimnsree369@gmail.com>
2024-07-17 23:14:40 +0530

    bug fixes in swap functionality

--- src/main/java/learn/dsa/Sorting/QuickSort.java
+++ src/main/java/learn/dsa/Sorting/QuickSort.java
@@ -6,7 +6,7 @@ public class QuickSort {
         int arr[] = {2,5,6,8,13,21,56,34,22,58,16};
 //        int[] arr = {18, 80, 30, 90, 40, 50, 70};
 //        int[] arr = {5, 3, 8, 4, 2, 7, 1, 10};
-        lomutoSort(arr, 0, arr.length-1);
+        hoareSort(arr, 0, arr.length-1);
         for(int a: arr){
             System.out.print(a+" ");
         }
@@ -21,11 +21,14 @@ public class QuickSort {
                 arr[j] = arr[j] + arr[i];
                 arr[i] = arr[j] - arr[i];
                 arr[j] = arr[j] - arr[i];
+//                int temp = arr[i];
+//                arr[i] = arr[j];
+//                arr[j] = temp;
             }
         }
-        arr[i+1] = arr[high] + arr[i + 1];
-        arr[high] = arr[high] - arr[i + 1];
+        arr[high] = arr[high] + arr[i + 1];
         arr[i+1] = arr[high] - arr[i + 1];
+        arr[high] = arr[high] - arr[i + 1];
     }
 
     public static void hoareSort(int arr[], int l, int h){
